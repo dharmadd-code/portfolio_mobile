aa99779d00092ec45506667a1845277a2ef97cd9
dharmadd-code <115447670+dharmadd-code@users.noreply.github.com>
2024-11-17 15:55:42 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// Simple JavaScript to scroll to sections smoothly when clicking the navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+});
